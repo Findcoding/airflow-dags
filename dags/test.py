910bf3a043344b2d6d266013059687926ee33af3
FindCoding <bijendarprasad772244@gmail.com>
2026-01-13 10:45:31 +0000

    modify files

--- dags/test.py
+++ dags/test.py
@@ -1,1 +1,18 @@
-print("hello");
+from airflow.decorators import dag, task
+from datetime import datetime
+
+@dag(
+    dag_id="hello_world_taskflow",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+)
+def hello_world():
+
+    @task
+    def say_hello():
+        print("Hello World from Airflow!")
+
+    say_hello()
+
+hello_world()
